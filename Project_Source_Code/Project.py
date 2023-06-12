0598dec60447f918511229aad51080073801bfea
Haleshot <hari.leo03@gmail.com>
2023-06-12 11:54:59 +0530

    Update relative directory

--- Project_Source_Code/Project.py
+++ Project_Source_Code/Project.py
@@ -862,8 +862,7 @@ FlightID = 0 # Global variable for accessing chosen Flight ID of customer across
 class Payment_Booking(QDialog):
     def __init__(self):
         super(Payment_Booking, self).__init__()
-        loadUi(r"F:\New_Laptop_Documents\NMIMS_College_Docs\2nd_Year\1st_Semester\DBMS\Project\Project_Source_Code\Payment_Booking_Screen.ui", self)
-        # Relative Path - loadUi(r"Project_Source_Code\Payment_Booking_Screen.ui", self")
+        loadUi(r"Project_Source_Code\Payment_Info_Screen.ui", self)
         
         self.User_Input_Flight_ID.clicked.connect(self.Display_User_Flight_ID)
         self.Proceed_To_Summary_Button.clicked.connect(self.gotoSummary_Information)
