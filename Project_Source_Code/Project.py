78c946a7d7faf88c7c3fccdfeef7bd7b2a3532d9
Haleshot <hari.leo03@gmail.com>
2023-07-04 11:59:17 +0530

    Update Indivo function name

--- Project_Source_Code/Project.py
+++ Project_Source_Code/Project.py
@@ -809,20 +809,20 @@ class Indivo_Admin_Options(QDialog):
         loadUi(r"Project_Source_Code\Indivo_Admin_Options.ui", self)
 
         self.Check_Flights_Button.clicked.connect(self.gotoCheckFlights)
-        self.Payments_Button.clicked.connect(self.gotoNistaraPayments)
-        self.Cancellations_Button.clicked.connect(self.gotoNistaraCancellations)
+        self.Payments_Button.clicked.connect(self.gotoIndivoPayments)
+        self.Cancellations_Button.clicked.connect(self.gotoIndivoCancellations)
 
     def gotoCheckFlights(self):
         indivo = Indivo_Flight_Options()
         widget.addWidget(indivo)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def gotoNistaraPayments(self):
+    def gotoIndivoPayments(self):
         payments_info = Indivo_Payments_Info()
         widget.addWidget(payments_info)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
-    def gotoNistaraCancellations(self):
+    def gotoIndivoCancellations(self):
         cancellation_info = Indivo_Cancellation_Info()
         widget.addWidget(cancellation_info)
         widget.setCurrentIndex(widget.currentIndex() + 1)
